2e496a0e9fbae6f85458f085da42bca10f173ee4
PavithraGadamsetty <122758134+PavithraGadamsetty@users.noreply.github.com>
2023-01-27 18:05:56 -0800

    Widening program

---
+++ javaproject/corejava/src/main/java/com/org/google/Widening.java
@@ -0,0 +1,10 @@
+package com.org.google;
+
+public class Widening {
+    public static void main(String[] args) {
+        int a = 9;
+        double b = a;
+        System.out.println(b);
+
+    }
+}
