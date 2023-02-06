37108b0c79035b810f6ba2b9675e69a244d7c498
Pavithra Gadamsetty <ap.gadamsetty@gmail.com>
2023-02-06 11:15:50 -0800

    Create narrowing.java
    narrowing

---
+++ javaproject/corejava/src/main/java/com/org/google/narrowing.java
@@ -0,0 +1,9 @@
+package com.org.google;
+
+public class narrowing {
+    public static void main(String[] args) {
+        double a=688.68d;
+        int b= (int) a;
+        System.out.println(b);
+    }
+}
